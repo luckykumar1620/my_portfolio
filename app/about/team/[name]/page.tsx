d02982f2392d76f9069ec4ffc88a144ae4064fea
Lucky Kumar <lucky9110132241@gmail.com>
2026-01-30 02:24:12 +0530

    fix params type

--- app/about/team/[name]/page.tsx
+++ app/about/team/[name]/page.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
 
-const dynamicPage = async ({params}) => {
+type PageProps = {
+  params: {
+    name: string
+  }
+}
+
+const dynamicPage = async ({params}:PageProps) => {
     const name=(await params).name
   return ( 
     <div>
